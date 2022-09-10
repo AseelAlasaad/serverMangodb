d5aa9b67d3eff5193bdc37288c29e9cb95538217
AseelAlasaad <aseelalasaad@gmail.com>
2022-09-10 21:03:10 +0300

    push to signin/signup

--- auth/route.js
+++ auth/route.js
@@ -34,10 +34,5 @@ userRouter.post('/signin', basicAuth, async (req, res) => {
     res.status(200).json(userInfo);
 })
 
-userRouter.get('/user',  (req, res) => {
-    userModel.find({}, (error, data) => {
-        if (error) console.log(`error reading from the db: ${error}`);
-        else res.send(data);
-})
-})
+
 module.exports = userRouter;
